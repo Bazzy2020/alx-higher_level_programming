f7edaeb199980b5c212ceff43620ea660197c1f8
Bazzy2020 <kene.anumba@yahoo.co.uk>
2022-06-02 20:13:28 -0700

    The commit for 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,6 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     from calculator_1 import *
     count = len(argv)
+
     if count != 4:
         print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
@@ -22,7 +23,7 @@ if __name__ == "__main__":
 
     def sub_():
         total = sub(num1, num2)
-        print("{:d} + {:d} = {:d}".format(num1, num2, total))
+        print("{:d} - {:d} = {:d}".format(num1, num2, total))
         return total
 
     def mul_():
